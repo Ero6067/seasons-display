3f726edf7313ae18179caf721f99bff2d31521a1
Kris Lafrance <kris.lafrance6067@gmail.com>
2019-03-08 10:10:14 -0800

    Adding state
    Added the state to the app. For this app the state that we are using in this instance is tracking the player Latitude. Added comments to the app as well

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <div> Hi there!</div>;
+class App extends React.Component {
+  constructor(props) {
+    //calls parents constructor function
+    super(props);
 
-};
+    //initialzed state with a key:value pair
+    //this is the only time we do direct assignment to this.state (this.state = x)
+    this.state = {lat: null};
+
+    //get the users current location as soon as the app launches
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setState to change a states value
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  //React says we have to define render!!
+  render()  {
+        return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(
   <App />,

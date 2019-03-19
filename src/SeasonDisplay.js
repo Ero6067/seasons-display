580b63562bfa1a4f9d443a67857ed45d96bb4c7f
Kris Lafrance <kris.lafrance6067@gmail.com>
2019-03-19 11:03:20 -0700

    Adding month detection
    Adding code that allows the app to detect what month it is as well as to display on the screen a string based on what season it it.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,24 @@
-Import React from 'react';
+import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const getSeason = (lat, month)  =>  {
+  if (month > 2 && month < 9) {
+    //Ternary exression
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
 
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const seasonText = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'
+  //console.log(season);
+
+  return (
+  <div>
+    <h1>{seasonText} </h1>
+  </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
 
@@ -25,7 +26,7 @@ componentDidUpdate()  {
     }
 
      if(!this.state.errorMessage && this.state.lat) {
-       return <div>Latitude: {this.state.lat}</div>;
+       return <SeasonDisplay lat = {this.state.lat} />
      }
 
      return <div>Loading!!</div>;
